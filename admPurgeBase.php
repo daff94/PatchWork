6cb060138b1b9446972a11450d8681f37832cc26
daff94 <yoann.tessier@gmail.com>
2024-12-02 19:58:30 +0100

    On ne peut pas purger la version en cours

--- admPurgeBase.php
+++ admPurgeBase.php
@@ -7,6 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/main.css" rel="stylesheet">
     <link href="css/modale.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 
@@ -31,25 +32,37 @@ $con = mysqli_connect("localhost","root","","patchwork");
 if (mysqli_connect_errno())
   {   echo "Failed to connect to MySQL: " . mysqli_connect_error();  }
 
-$sqlSelectionTableHistorique = "select * from refversion;";
+// Sélection du choix des versions sauf celle en cours
+$sqlSelectionTableHistorique = "select * from refversion where refencours <> 'X'";
 $sqlVersionactuelle = "select * from refversion where refencours = 'X'";
 $resultVersionactuelle = $con->query($sqlVersionactuelle);
 $resultSelectionTableHistorique = $con->query($sqlSelectionTableHistorique);
 
 $Versionactuelle = $resultVersionactuelle->fetch_assoc();
-echo "Version Actuelle : " . $Versionactuelle['refversion'] . " en date du " . $Versionactuelle['refdate'] . "<br>";
 
-if ($resultSelectionTableHistorique->num_rows > 0) {
-    echo "<select name='choixVersion' id='choixVersion-select'>";
-    while($rowVersion = $resultSelectionTableHistorique->fetch_assoc()) {
-		// Affichage d'un liste de choix unique de la version à purger
-		echo "<option value=" . $rowVersion['refversion'] . ">" . "Version : " . $rowVersion['refversion'] . " en date du " . $rowVersion['refdate'] . "</option>";
+echo "<div class='container'>";
+echo "<div>";
+echo "Version Actuelle : " . $Versionactuelle['refversion'] . " en date du " . $Versionactuelle['refdate'] ;
+echo "</div>";
+
+    echo "<div>";
+    if ($resultSelectionTableHistorique->num_rows > 0) {
+        echo "<select class='form-select' name='choixVersion' id='choixVersion-select'>";
+        while($rowVersion = $resultSelectionTableHistorique->fetch_assoc()) {
+            // Affichage d'un liste de choix unique de la version à purger
+            echo "<option value=" . $rowVersion['refversion'] . ">" . "Version : " . $rowVersion['refversion'] . " en date du " . $rowVersion['refdate'] . "</option>";
+        }
+        echo "</select>";
+        } else {
+            echo "Aucun résultat";
     }
-    echo "</select>";
-    } else {
-        echo "Aucun résultat";
-}
+    echo "</div>";
 
+    echo "<div style='text-align: center;'>";
+    echo "<button class='btn btn-danger' type='button'>Supprimer!</button>";
+    echo "</div>";
+
+echo "</div>";
 $con->close();
 
 ?>
