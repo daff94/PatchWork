1ce67f6fedeed5faed25f78635c84165c722cc9b
TESSIERY <yoann.tessier@etat.ge.ch>
2024-12-19 08:35:32 +0100

    Margin-top sur la page d'accueil

--- index.php
+++ index.php
@@ -15,23 +15,18 @@
 <?php include 'menu_principal.html' ?>
 
 <div class="container-fluid">
-  <div class="row" >
+  <div class="row" style="margin-top: 15px">
     <div id="TextePresentation" class="col-8">
       <p>Et quoniam mirari posse quosdam peregrinos existimo haec lecturos forsitan, si contigerit, quamobrem cum oratio ad ea monstranda deflexerit quae Romae gererentur, nihil praeter seditiones narratur et tabernas et vilitates harum similis alias, summatim causas perstringam nusquam a veritate sponte propria digressurus.</p>
     </div>
     <div class="col-4"><img src="images/presentation/mockup02.jpg" class="rounded img-thumbnail" ></div>
   </div>
-</div>
-
-<div class="container-fluid">
-  <div class="row">
+  <div class="row" style="margin-top: 15px">
     <div class="col-4"><img src="images/presentation/mockup01.jpg" class="rounded img-thumbnail" ></div>
     <div class="col-4"><img src="images/presentation/mockup02.jpg" class="rounded img-thumbnail" ></div>
     <div class="col-4"><img src="images/presentation/mockup03.jpg" class="rounded img-thumbnail" ></div>
   </div>
-</div>  
-
-
+</div>
   
 <!-- Code pour la fenetre modale - A propos-->
 <?php include 'modale.html' ?>
